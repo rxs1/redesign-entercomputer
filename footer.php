3abcf69c31e913b47613ac3538751d916afb1ad3
root <root@sr.saufi>
2016-12-11 20:39:03 +0700

    footer

---
+++ footer.php
@@ -0,0 +1,64 @@
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="col-md-4">
+					<p>
+					Alamat:
+					</p>
+					<p>
+					Mangga Dua Mall Lt.5 Blok C No. 101.106
+					</p>
+					<p>
+					Jakarta Pusat 10730
+					</p>
+					<p>
+						<i class="glyphicon glyphicon-earphone"></i> (021) 3043 033
+					</p>
+										<p>
+						<i class=" glyphicon glyphicon-envelope"></i> sales@enterkomputer.com
+					</p>
+					
+				</div>
+
+				<div class="col-md-4">
+					<ul class="nav nav-pills nav-stacked">
+			          <li><a href="#">Simulasi Harga</a></li>
+			          <li><a href="#">Paket Komputer</a></li>
+			          <li><a href="#">Beli Paket</a></li>          
+			        </ul>
+				</div>
+
+				<div class="col-md-4">
+					<ul class="nav nav-pills nav-stacked">
+			          <li><a href="#">Bantuan</a></li>
+			          <li><a href="#">Hubungi Kami</a></li>
+			          <li><a href="#">Syarat dan Kebijakan Privasi</a></li>          
+			        </ul>
+				</div>
+			</div>
+
+
+
+		</div>
+
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="col-md-4 social-media">
+
+					<i class="fa fa-facebook-square" aria-hidden="true"></i>
+					<i class="fa fa-twitter-square" aria-hidden="true"></i>
+					<i class="fa fa-instagram" aria-hidden="true"></i>
+
+				</div>
+				<div class="col-md-4">
+				 <center>
+				 <p>Copyright @ Enter Komputer 2016</p>
+				 <p>All Right reserved</p>
+				 </center>
+				</div>
+			</div>	
+		</div>
+
+	</div>
+</footer>
---
+++ processor.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<?php
+	$title ='Processor' ;
+	include 'head.php';
+?>
+
+</head>
+<body>
+<?php include'footer.php';?>
+</body>
+</html>
