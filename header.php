a8e571826e3c0fb8cfab7986ba7f5be2ebd6299e
Filza Valorisantyo <filza_79@yahoo.com>
2016-12-12 06:43:02 +0700

    ram done

--- header.php
+++ header.php
@@ -6,13 +6,13 @@
 	<div id="menuBar" class="col-md-6">
 		<div id="tabContainer">
 			<ul id="topTab">
-				<li><a href="homepage.html">Home</a></li>
-				<li><a href="processor.html">Processor</a></li>
-				<li><a href="vga.html">VGA</a></li>
-				<li><a href="ram.html">RAM</a></li>
-				<li><a href="storage.html">Storage</a></li>
-				<li><a href="pwersupply.html">Power Supply</a></li>
-				<li><a href="peripheral.html">Peripheral</a></li>
+				<li><a href="homepage.php">Home</a></li>
+				<li><a href="processor.php">Processor</a></li>
+				<li><a href="vga.php">VGA</a></li>
+				<li><a href="ram.php">RAM</a></li>
+				<li><a href="storage.php">Storage</a></li>
+				<li><a href="powersupply.php">Power Supply</a></li>
+				<li><a href="peripheral.php">Peripheral</a></li>
 			</ul>
 		</div>
 	</div>
