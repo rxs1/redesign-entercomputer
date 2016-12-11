27d62e6c67b0739aa0bb45976adc80192e440b7d
Rifki Nurfaiz <rifkinurfaiz@gmail.com>
2016-12-12 01:25:33 +0700

    header, search fix

--- header.php
+++ header.php
@@ -1,36 +1,27 @@
 
 <div id="header" class="col-md-12">
 	<div id="logo" class="col-md-3">
-		<img src="assets/img/logo.png" alt="..." style="height:60px">
+		<h2>Enter Komputer</h2>
 	</div>
 	<div id="menuBar" class="col-md-6">
 		<div id="tabContainer">
 			<ul id="topTab">
-				<li><a href="homepage.php">Home</a></li>
-				<li><a href="processor.php">Processor</a></li>
-				<li><a href="vga.php">VGA</a></li>
-				<li><a href="ram.php">RAM</a></li>
-				<li><a href="storage.php">Storage</a></li>
-				<li><a href="pwersupply.php">Power Supply</a></li>
-				<li><a href="peripheral.php">Peripheral</a></li>
+				<li><a href="homepage.html">Home</a></li>
+				<li><a href="processor.html">Processor</a></li>
+				<li><a href="vga.html">VGA</a></li>
+				<li><a href="ram.html">RAM</a></li>
+				<li><a href="storage.html">Storage</a></li>
+				<li><a href="pwersupply.html">Power Supply</a></li>
+				<li><a href="peripheral.html">Peripheral</a></li>
 			</ul>
 		</div>
 	</div>
 	<div id="info" class="col-md-3">
-		<table>
-			<tr>
-				<td><i class="fa fa-phone"></i></td>
-				<td> &nbsp;(021) 3043 0333</td>
-			</tr>
-			<tr>
-				<td><i class="fa fa-envelope"></i></td>
-				<td> &nbsp;sales@enterkomputer.com</td>
-			</tr>
-			<tr>
-				<td><i class="fa fa-usd"></i></td>
-				<td> &nbsp;Simulasi Harga</td>
-			</tr>
-		</table>
+		<div>
+			<p><i class="fa fa-phone"></i>&nbsp;(021) 3043 0333</p>
+			<p><i class="fa fa-envelope"></i>&nbsp;sales@enterkomputer.com</p>
+			<p><i class="fa fa-usd"></i>&nbsp;Simulasi Harga</p>
+		</div>
 	</div>
 </div>
 
