3aab39082da6f1b5f7c0f0327b775966cfda0ff7
Rifki Nurfaiz <rifkinurfaiz@gmail.com>
2016-12-13 22:10:39 +0700

    fix header

--- header.php
+++ header.php
@@ -21,49 +21,44 @@
 			<p><i class="fa fa-envelope"></i>&nbsp;sales@enterkomputer.com</p>
 			
 			<p>
-			<!-- Split button -->
-				<div class="btn-group">
-				
-				  <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-				    <span style="float: right; margin-left: 0px"  class="">[3]</span>
-				    <span class="sr-only">Toggle Dropdown</span>
-				  </button>
-				    <a href="simulasi.php">
-				  <button type="button" class="btn btn-danger"><i class="fa fa-usd"></i>&nbsp;Simulasi Harga </button>
-				  </a>
-				  <ul class="dropdown-menu">
-				  	<div class="limit">
-					  	<li class="item">
-						   
-						   		<p calss='chart-name'>
-						  		Intel core i3 LGA 1151
-						  		</p>
+				<!-- Split button -->
+				<div>
+					<a href="simulasi.php">
+						<button type="button" class="btn btn-danger"><i class="fa fa-usd"></i>&nbsp;Simulasi Harga </button>
+					</a>
+					<button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						<span>[3]</span>
+						<span class="sr-only">Toggle Dropdown</span>
+					</button>
+					<ul class="dropdown-menu">
+						<div class="limit">
+							<li class="item">
+								<p calss='chart-name'>
+									Intel core i3 LGA 1151
+								</p>
 
-						  		<p calss='chart-price'>
-						  		Rp3.000.000 x <span class="qty">1</span><a href="#" "><i class="pull-right glyphicon glyphicon-trash btn btn-danger"></i></a>
-						  		</p>
-						  		<hr>
-						  		<p calss='chart-name'>
-						  		 AMD Athlon X2
-						  		</p>
-						  		<p calss='chart-price'>
-						  		Rp3.002.000 x <span class="qty">1</span><a href="#" "><i class="pull-right glyphicon glyphicon-trash btn btn-danger"></i></a>
-						  		</p>
-						  		<hr>
-						  		<p calss='chart-name'>
-						  		Asus GTX 1060 6GB DDR5 Dual OC
-						  		</p>
-						  		<p calss='chart-price'>
-						  		Rp4.750.000 x <span class="qty">1</span><a href="#" "><i class="pull-right glyphicon glyphicon-trash btn btn-danger"></i></a>
-						  		</p> 
-						  		<hr>
-
-						   	
-					   	</li>
-
-				   	</div>
-				   	<li><p id="totalharga">Total Harga : Rp10.752.000</p> </li>
-				  </ul>
+								<p calss='chart-price'>
+									Rp3.000.000 x <span class="qty">1</span><a href="#" "><i class="pull-right glyphicon glyphicon-trash btn btn-danger"></i></a>
+								</p>
+								<hr>
+								<p calss='chart-name'>
+									AMD Athlon X2
+								</p>
+								<p calss='chart-price'>
+									Rp3.002.000 x <span class="qty">1</span><a href="#" "><i class="pull-right glyphicon glyphicon-trash btn btn-danger"></i></a>
+								</p>
+								<hr>
+								<p calss='chart-name'>
+									Asus GTX 1060 6GB DDR5 Dual OC
+								</p>
+								<p calss='chart-price'>
+									Rp4.750.000 x <span class="qty">1</span><a href="#" "><i class="pull-right glyphicon glyphicon-trash btn btn-danger"></i></a>
+								</p> 
+								<hr>
+							</li>
+						</div>
+						<p id="totalharga">Total Harga : Rp10.752.000</p> </li>
+					</ul>
 				</div>
 			</p>
 		</div>
