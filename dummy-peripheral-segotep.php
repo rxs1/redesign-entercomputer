9429fb5595f8c46fdb3b9c4867c57987bc408c38
Rifki Nurfaiz <rifkinurfaiz@gmail.com>
2016-12-13 11:07:44 +0700

    fix bug peripheral

--- dummy-peripheral-segotep.php
+++ dummy-peripheral-segotep.php
@@ -70,94 +70,95 @@
 			</div>
 		</div>
 	</a>
+</div>
 
-	<!-- Modal -->
+<!-- Modal -->
 <div id="myModal" class="modal fade" role="dialog">
-  <div class="modal-dialog modal-lg">
-    <!-- Modal content-->
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title"> SEGOTEP GAMING CASE SPRINT Side Window Include Front Led Fan & USB 3.0</h4>
-      </div>
-      <div class="modal-body">
+	<div class="modal-dialog modal-lg">
+		<!-- Modal content-->
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal">&times;</button>
+				<h4 class="modal-title"> SEGOTEP GAMING CASE SPRINT Side Window Include Front Led Fan & USB 3.0</h4>
+			</div>
+			<div class="modal-body">
 				
-			
-			<div class="row">
-				<div class="col-md-6">
-					<div id="myCarousel" class="carousel slide" data-ride="carousel">
-					  <!-- Indicators -->
-					  <ol class="carousel-indicators">
-					    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-					    <li data-target="#myCarousel" data-slide-to="1"></li>
-					    <li data-target="#myCarousel" data-slide-to="2"></li>
-					    <li data-target="#myCarousel" data-slide-to="3"></li>
-					  </ol>
+				
+				<div class="row">
+					<div class="col-md-6">
+						<div id="myCarousel" class="carousel slide" data-ride="carousel">
+							<!-- Indicators -->
+							<ol class="carousel-indicators">
+								<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+								<li data-target="#myCarousel" data-slide-to="1"></li>
+								<li data-target="#myCarousel" data-slide-to="2"></li>
+								<li data-target="#myCarousel" data-slide-to="3"></li>
+							</ol>
 
-					  <!-- Wrapper for slides -->
-					  <div class="carousel-inner" role="listbox">
-					    <div class="item active">
-					      <img src="assets/img/peripheral-1.jpg" alt="segotep">
-					    </div>
+							<!-- Wrapper for slides -->
+							<div class="carousel-inner" role="listbox">
+								<div class="item active">
+									<img src="assets/img/peripheral-1.jpg" alt="segotep">
+								</div>
 
-					    <div class="item">
-					      <img src="assets/img/peripheral-1.jpg" alt="segotep">
-					    </div>
+								<div class="item">
+									<img src="assets/img/peripheral-1.jpg" alt="segotep">
+								</div>
 
-					    <div class="item">
-					      <img src="assets/img/peripheral-1.jpg" alt="segotep">
-					    </div>
+								<div class="item">
+									<img src="assets/img/peripheral-1.jpg" alt="segotep">
+								</div>
 
-					    <div class="item">
-					      <img src="assets/img/peripheral-1.jpg" alt="segotep">
-					    </div>
-					  </div>
+								<div class="item">
+									<img src="assets/img/peripheral-1.jpg" alt="segotep">
+								</div>
+							</div>
 
-					  <!-- Left and right controls -->
-					  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-					    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-					    <span class="sr-only">Previous</span>
-					  </a>
-					  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-					    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-					    <span class="sr-only">Next</span>
-					  </a>
+							<!-- Left and right controls -->
+							<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+								<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+								<span class="sr-only">Previous</span>
+							</a>
+							<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+								<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+								<span class="sr-only">Next</span>
+							</a>
+						</div>
 					</div>
-				</div>
 
-				<div class="col-md-6">
-					<p>
-						Untuk PC
-					</p>
-					<hr>
-					<p>
-						SEGOTEP GAMING CASE SPRINT
-					</p>
-					<hr>
-					<p>
-						Side Window Include Front Led Fan & USB 3.0
-					</p>
-					<hr>
-					<p class="stock">
-						Stock:31
-					</p>
-					<hr>
-					<p>
+					<div class="col-md-6">
+						<p>
+							Untuk PC
+						</p>
+						<hr>
+						<p>
+							SEGOTEP GAMING CASE SPRINT
+						</p>
+						<hr>
+						<p>
+							Side Window Include Front Led Fan & USB 3.0
+						</p>
+						<hr>
+						<p class="stock">
+							Stock:31
+						</p>
+						<hr>
+						<p>
 
-					<h3>Rp460.000</h3> 
-					<a class="pull-right class btn btn-success"> Tambahkan ke Simulasi</a>
-					
-					</p>
+							<h3>Rp460.000</h3> 
+							<a class="pull-right class btn btn-success"> Tambahkan ke Simulasi</a>
+							
+						</p>
+
+					</div>
 
 				</div>
 
 			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+			</div>
+		</div>
 
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-
-  </div>
+	</div>
 </div>
